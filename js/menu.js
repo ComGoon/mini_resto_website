37a6ca671b97a893a6846fc4d1ea3ec4d736ee1d
Com Goon <comgoon@Coms-MacBook-Pro.local>
2019-01-30 12:20:46 +0100

    imporvi

--- js/menu.js
+++ js/menu.js
@@ -1,11 +1,41 @@
-var plats=document.querySelectorAll('img');
-plats.forEach(element => {
-    element.addEventListener('mouseout', function(e){
-    console.log(e.target.nextElementSibling.classList.toggle('active'));
-})
+document.addEventListener("DOMContentLoaded", function() {
+  var plats = document.querySelectorAll(".card-deck .card");
+  console.log(plats);
 
-    element.addEventListener('mouseover', function(e){  
-    console.log(e.target.nextElementSibling.classList.toggle('active'));
-   // e.target.nextElementSibling.style.background = '#fff';
-  })
+  //setting up opacity when mouseover or mouseout
+  plats.forEach(element => {
+    // console.log(element);
+
+    element.addEventListener("mouseover", function(e) {
+      if (e.target.classList[0] == "card-img-top") {
+        e.target.style.opacity = "1";
+        e.target.style.border = "3px solid #733279";
+      } else if (e.target.classList[0] == "card-text") {
+        e.target.parentElement.previousSibling.previousSibling.style.opacity =
+          "1";
+      } else if (e.target.classList[0] == "card-title") {
+        e.target.parentElement.previousSibling.previousSibling.style.opacity =
+          "1";
+      } else if (e.target.classList[0] == "card-body") {
+        e.target.previousSibling.previousSibling.style.opacity = "1";
+      }
+      //   card-text card-title  card-body
+    });
+
+    element.addEventListener("mouseout", function(e) {
+      if (e.target.classList[0] == "card-img-top") {
+        e.target.style.opacity = ".7";
+        e.target.style.border = "none";
+      } else if (e.target.classList[0] == "card-text") {
+        e.target.parentElement.previousSibling.previousSibling.style.opacity =
+          ".7";
+      } else if (e.target.classList[0] == "card-title") {
+        e.target.parentElement.previousSibling.previousSibling.style.opacity =
+          ".7";
+      } else if (e.target.classList[0] == "card-body") {
+        e.target.previousSibling.previousSibling.style.opacity = ".7";
+      }
+      //   card-text card-title  card-body img
+    });
+  });
 });
